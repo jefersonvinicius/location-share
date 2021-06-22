f5732970e6d3259644718ecbddfcf3cd68fe4c83
Jeferson Vinícius <jeferson.viniciuscrc@gmail.com>
2021-06-22 09:33:24 -0300

    Init socket.io tests

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,34 @@
 import app from './app';
+import http from 'http'
 import { createConnection } from 'typeorm';
+import { Server, Socket } from 'socket.io';
+
+const httpServer = http.createServer(app)
+
+enum SocketEvents {
+    NewLocation = 'new-location'
+}
+
+const io = new Server(httpServer, {
+    cors: {
+        origin: '*'
+    }
+})
+io.on('connection', (socket: Socket) => {
+    console.log(`${socket.id} connected`)
+    socket.on('disconnect', () => {
+        console.log(`${socket.id} disconnected`)
+    })
+    socket.on(SocketEvents.NewLocation, handleNewLocation)
+
+    function handleNewLocation(location: any) {
+        console.log('NEW LOCATION: ', location)
+    }
+})
 
 async function bootstrap() {
     await createConnection();
-    app.listen(3333, () => {
+    httpServer.listen(3333, () => {
         console.log('Serving http://localhost:3333');
     });
 }
