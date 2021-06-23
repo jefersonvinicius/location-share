9a695d3468ba2507b8e2e9212732ea6c7f65a88f
Jeferson Vinícius <jeferson.viniciuscrc@gmail.com>
2021-06-23 09:34:10 -0300

    Add new user event

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,35 +1,52 @@
 import app from './app';
-import http from 'http'
+import http from 'http';
 import { createConnection } from 'typeorm';
 import { Server, Socket } from 'socket.io';
 
-const httpServer = http.createServer(app)
+const httpServer = http.createServer(app);
 
 enum SocketEvents {
-    NewLocation = 'new-location'
+    NewLocation = 'new-location',
+    NewUser = 'new-user',
+    UserDisconnected = 'user-disconnected',
 }
 
 const io = new Server(httpServer, {
     cors: {
-        origin: '*'
-    }
-})
+        origin: '*',
+    },
+});
+
+type User = {
+    id: string;
+    username: string;
+};
+
 io.on('connection', (socket: Socket) => {
-    console.log(`${socket.id} connected`)
-    socket.on('disconnect', () => {
-        console.log(`${socket.id} disconnected`)
-    })
-    socket.on(SocketEvents.NewLocation, handleNewLocation)
+    socket.on(SocketEvents.NewUser, handleNewUser);
+    socket.on(SocketEvents.NewLocation, handleNewLocation);
+
+    socket.on('disconnect', handleUserDisconnect);
 
     function handleNewLocation(location: any) {
-        console.log('NEW LOCATION: ', location)
+        console.log('NEW LOCATION: ', location);
     }
-})
+
+    function handleNewUser(user: User) {
+        console.log(`${user.username} connected`);
+        socket.broadcast.emit(SocketEvents.NewUser, user);
+    }
+
+    function handleUserDisconnect() {
+        console.log(`${socket.id} disconnected`);
+        socket.broadcast.emit(SocketEvents.UserDisconnected, { socketId: socket.id });
+    }
+});
 
 async function bootstrap() {
     await createConnection();
-    httpServer.listen(3333, () => {
-        console.log('Serving http://localhost:3333');
+    httpServer.listen(3333, '0.0.0.0', () => {
+        console.log('Serving http://0.0.0.0:3333');
     });
 }
 
